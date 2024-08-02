02f77f33ad9225fc3b8229941b2a0d6ce78a8e7a
Office Repo <aezaz@microsysx.com>
2024-08-02 20:30:57 +0500

    New Changes!

--- src/CategoryForm.js
+++ src/CategoryForm.js
@@ -1,132 +1,163 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+import { TextField, Button, Box, Alert } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import MyNavBar from "./mynavbar";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 const CategoryPage = () => {
-    const tokenAvailable = localStorage.getItem('token');
+  const tokenAvailable = localStorage.getItem('token');
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            catrgoryName: data.get('category'),
-            description: data.get('description'),
-            image: data.get('image'),
-            imageUrl: data.get('imageUrl'),
+  const [successMessage, setSuccessMessage] = React.useState("");
+
+  const validationSchema = Yup.object({
+    catrgoryName: Yup.string().required('Category Name is required'),
+    description: Yup.string().required('Description is required'),
+    image: Yup.string().required('Image is required'),
+    imageUrl: Yup.string().required('Image URL is required'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      catrgoryName: '',
+      description: '',
+      image: '',
+      imageUrl: '',
+    },
+
+    validationSchema: validationSchema,
+    onSubmit: async (values, { resetForm }) => {
+      console.log(values);
+      // Handle form submission
+      try {
+        const response = await axios.post('https://localhost:7094/api/Category/Add', values, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         });
-    
-        const dataObj = {
-            catrgoryName: data.get('category'),
-            description: data.get('description'),
-            image: data.get('image'),
-            imageUrl: data.get('imageUrl'),
-        };
-    
-        try {
-          const response = await axios.post('https://localhost:7094/api/Category/Add', dataObj, {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-    
-          if (response.status === 200) {
-            // Handle successful login
-            console.log(`Category created successful! Welcome ${response.data}`);
-            
-            
-            
-            //navigate('/category');
-            // Redirect or update the UI accordingly
-          } else {
-            // Handle other responses
-            console.log(`Creation failed: ${response.data.message}`);
-          }
-        } catch (error) {
-          // Handle errors
-          console.log(`An error occurred: ${error.response ? error.response.data.message : error.message}`);
+
+        if (response.status === 200) {
+          // Handle successful login
+          console.log(`Category created successful! Welcome ${response.data}`);
+
+
+
+          //navigate('/category');
+          // Redirect or update the UI accordingly
+        } else {
+          // Handle other responses
+          console.log(`Creation failed: ${response.data.message}`);
         }
-      };
+      } catch (error) {
+        // Handle errors
+        console.log(`An error occurred: ${error.response ? error.response.data.message : error.message}`);
+      }
+
+      resetForm();  //Reset input values
+
+      setSuccessMessage("Data saved Successfully!!!");
+
+      setTimeout(()=>setSuccessMessage(""), 3000);
+    },
+  });
 
 
-    console.log(tokenAvailable);
-    return ( 
-        tokenAvailable ?
-        (<div>
-            <MyNavBar />
-            THis is CategoryPage
-            <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >          
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="category"
-                label="Category Name"
-                name="category"
-                autoComplete="category"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="description"
-                label="Description"                
-                id="description"
-                autoComplete="description"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="image"
-                label="Image"                
-                id="image"
-                autoComplete="image"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="imageUrl"
-                label="ImageUrl"                
-                id="imageUrl"
-                autoComplete="imageUrl"
-              />
-              
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Create Category!
-              </Button>
-              
-              
-            </Box>
-            </Box>
-        </div>) :
-        (<div>
-            <h1>
-                You need Logintoken to view this page! 
-            </h1>
-            </div>)
-     );
+
+  console.log(tokenAvailable);
+  return (
+    tokenAvailable ?
+      (<div>
+        <MyNavBar />
+        THis is CategoryPage
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box component="form" noValidate onSubmit={formik.handleSubmit} sx={{ mt: 1 }}>
+
+            {successMessage && <Alert severity="success">{successMessage}</Alert>}
+
+            <TextField
+              margin="normal"
+              fullWidth
+              id="category"
+              label="Category Name"
+              name="catrgoryName"
+              autoComplete="catrgoryName"
+              autoFocus
+              value={formik.values.catrgoryName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.catrgoryName && Boolean(formik.errors.catrgoryName)}
+              helperText={formik.touched.catrgoryName && formik.errors.catrgoryName}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="description"
+              label="Description"
+              name="description"
+              autoComplete="description"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.description && Boolean(formik.errors.description)}
+              helperText={formik.touched.description && formik.errors.description}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="image"
+              label="Image"
+              name="image"
+              autoComplete="image"
+              value={formik.values.image}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.image && Boolean(formik.errors.image)}
+              helperText={formik.touched.image && formik.errors.image}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="imageUrl"
+              label="Image URL"
+              name="imageUrl"
+              autoComplete="imageUrl"
+              value={formik.values.imageUrl}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.imageUrl && Boolean(formik.errors.imageUrl)}
+              helperText={formik.touched.imageUrl && formik.errors.imageUrl}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Create Category!
+            </Button>
+
+
+          </Box>
+        </Box>
+      </div>) :
+      (<div>
+        <h1>
+          You need Logintoken to view this page!
+        </h1>
+      </div>)
+  );
 }
- 
+
 export default CategoryPage;
